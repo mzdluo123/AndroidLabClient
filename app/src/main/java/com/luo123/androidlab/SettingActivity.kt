002ad99b7ad6a504c067dd3bc66c4d1a76d51d89
luo123 <luo2314572588@gmail.com>
2019-12-17 15:39:36 +0800

    彩蛋

--- app/src/main/java/com/luo123/androidlab/SettingActivity.kt
+++ app/src/main/java/com/luo123/androidlab/SettingActivity.kt
@@ -8,11 +8,17 @@ import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : AppCompatActivity() {
 
+    var click = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
         setting_card_view.setOnClickListener {
             Toast.makeText(this,"ο(=•ω＜=)ρ⌒☆",Toast.LENGTH_SHORT).show()
+            click++
+            if (click == 5){
+                egg_imageView.visibility = View.VISIBLE
+                textView.visibility = View.INVISIBLE
+            }
         }
     }
 }
